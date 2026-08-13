b98b935945095cd5a5e5132db638a3d9cc29fbf8
Navdeep <navdeeprana444@gmail.com>
2026-08-14 00:45:51 +0530

    Configure app for Vercel deployment

--- server.js
+++ server.js
@@ -84,6 +84,10 @@ app.use((err, req, res, next) => {
     next();
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;
